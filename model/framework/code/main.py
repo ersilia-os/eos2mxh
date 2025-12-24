95c60eee5cdbd5a237480a11931913b139130fb1
gemmaturon <gemma@ersilia.io>
2025-12-24 11:26:59 +0100

    Change output data type to np.float32 in write_out [skip ci]
    Updated write_out function to use np.float32 for output data type.

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,17 +2,11 @@
 import os
 import sys
 import numpy as np
-import numpy.typing as npt
-import json
-import csv
-from typing import List
 from ersilia_pack_utils.core import write_out, read_smiles
-
 from signaturizer3d import CCSpace, Signaturizer
 
 root = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(root)
-
 
 # current file directory
 checkpoints_dir = os.path.join(root, "..", "..", "checkpoints")
@@ -26,9 +20,7 @@ DATASETS = [i+j for i in "B" for j in "12345"]
 def get_model_path(ds):
     return os.path.join(checkpoints_dir, "{}_split3.pt".format(ds))
 
-
 def predict(smiles_list):
-
     # For each space, get 3 signatures
     results = {}
     for ds in DATASETS:
@@ -36,14 +28,12 @@ def predict(smiles_list):
         signaturizer = Signaturizer(space=ds, local_weights_path= path)
         signatures = signaturizer.infer_from_smiles(smiles_list)
         results[ds]=signatures
-
     # For each space, store the 3 signatures
     output = [[] for _ in range(len(smiles_list))]
     for ds in DATASETS:
         output[0].extend(results[ds][0])
         output[1].extend(results[ds][1])
         output[2].extend(results[ds][2])
-
     # to numpy array
     output = np.array(output)
     return output
@@ -54,5 +44,4 @@ output = predict(smiles)
 
 header = [f"{ds.lower()}_{r:03d}" for ds in DATASETS for r in range(128)]
 
-
-write_out(output,header,output_file,'float32')
+write_out(output,header,output_file,np.float32)
